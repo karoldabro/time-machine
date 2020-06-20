64dba52fcad1420a2db971ed8d09b4580a33c16d
Karol Dąbrowski <karol_dabrowski@interia.eu>
2020-06-20 13:02:30 +0200

    Split get results

--- src/Strategies/Resolvers/AbstractResolver.php
+++ src/Strategies/Resolvers/AbstractResolver.php
@@ -4,6 +4,8 @@ namespace Kdabrow\TimeMachine\Strategies\Resolvers;
 
 use Kdabrow\TimeMachine\DateChooser;
 use Kdabrow\TimeMachine\TimeMachine;
+use Kdabrow\TimeMachine\TimeTraveler;
+use Illuminate\Database\Eloquent\Model;
 use Kdabrow\TimeMachine\Database\Drivers\MysqlDriver;
 
 abstract class AbstractResolver
@@ -27,38 +29,48 @@ abstract class AbstractResolver
     public function resolve(): bool
     {
         $updated = [];
-        foreach ($this->timeMachine->getTravelers() as $traveller) {
+        foreach ($this->timeMachine->getTravelers() as $traveler) {
 
-            $model = app($traveller->getModel());
-
-            $query = $model->query();
-
-            if (!is_null($traveller->getConditions())) {
-                if (is_callable($traveller->getConditions())) {
-                    $query = call_user_func($traveller->getConditions(), $query, $updated);
-                }
-
-                if (is_array($traveller->getConditions())) {
-                    $query->where($traveller->getConditions());
-                }
-            }
-
-            $results = $query->get();
+            $results = $this->getItemsFromDb($traveler, $updated);
 
             foreach ($results as $result) {
                 $toUpdate = [];
-                foreach ((new MysqlDriver($traveller))->findUpdatableColumns() as $field) {
+                foreach ((new MysqlDriver($traveler))->findUpdatableColumns() as $field) {
                     if (is_callable($field->getValue())) {
                         $toUpdate[$field->getName()] = call_user_func($$field->getValue(), $result->{$field->getName()}, $field->getName(), $updated, $toUpdate);
                     } else {
                         $toUpdate[$field->getName()] = $this->query($result->{$field->getName()}, $field->getName());
                     }
                 }
-                $updated[] = $query->update($toUpdate);
+                $updated[] = $result->update($toUpdate);
             }
         }
 
         return true;
+    }
+
+    private function getItemsFromDb(TimeTraveler $traveler, &$updated)
+    {
+        $model = $this->resolveModel($traveler->getModel());
+
+        $query = $model->query();
+
+        if (!is_null($traveler->getConditions())) {
+            if (is_callable($traveler->getConditions())) {
+                $query = call_user_func($traveler->getConditions(), $query, $updated);
+            }
+
+            if (is_array($traveler->getConditions())) {
+                $query->where($traveler->getConditions());
+            }
+        }
+
+        return $query->get();
+    }
+
+    private function resolveModel($modelName): Model
+    {
+        return app($modelName);
     }
 
     public abstract function query($columnValue, string $columnName);
